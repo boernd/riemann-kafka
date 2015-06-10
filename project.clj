61a3c36d833a2397683eb00535847e34e236fa2b
bhois <bernd.hois@infonova.com>
2015-06-10 07:33:41 +0200

    version updates

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [riemann             "0.2.7"]
-                 [clj-kafka           "0.1.2-0.8"
+                 [riemann             "0.2.9"]
+                 [clj-kafka           "0.2.8-0.8.1.1"
                   :exclusions [org.slf4j/slf4j-log4j12
                                org.slf4j/slf4j-simple]]])
